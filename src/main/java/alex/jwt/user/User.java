7bd18ee6320dfe8d7ad3b9d1798759c70e07f44e
Alex Balak <alex.balak@outlook.com>
2024-10-27 21:10:39 +0100

    bootstrapped with mysql. Table creation working.

---
+++ src/main/java/alex/jwt/user/User.java
@@ -0,0 +1,24 @@
+package alex.jwt.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
