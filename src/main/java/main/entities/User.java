83cba17d82fb31cd82d3b8eab08116f0136ed984
Alex Balak <alex.balak@outlook.com>
2024-11-01 21:05:24 +0100

    Stopped the project cause I don't have the same methods in JWT packages. 'UserDetails' is missing.

--- src/main/java/main/entities/User.java
+++ src/main/java/main/entities/User.java
@@ -6,10 +6,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+
 @Data
 @Entity
 @Table(name = "user")
-public class User {
+public class User  {
 
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
