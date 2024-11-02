fa6170044342c080eacd7d43ddac000c4fb3d5d9
Alex Balak <alex.balak@outlook.com>
2024-11-02 12:21:11 +0100

    Working Basic http authentication

--- src/main/java/alex/oauth/config/SecurityConfig.java
+++ src/main/java/alex/oauth/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package alex.oauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,13 +16,25 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public InMemoryUserDetailsManager user() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("alex")
+                        .password("{noop}password")
+                        .authorities("read")
+                        .build()
+        );
+
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.
                 formLogin(AbstractHttpConfigurer::disable)// Disable the default login page
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/public/**").permitAll() // Allow public access to "/public" URLs
-                        .anyRequest().authenticated() // Require authentication for all requests
-                );
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Disable session
+                .httpBasic(Customizer.withDefaults());// Require authentication for all requests
+
         return http.build();
     }
 }
