fbe13e51fabff78312fcc91cc015f12cb35702f0
alexbalak21 <alex.balak@outlook.com>
2024-11-06 14:25:59 +0100

    Created Endpoints

--- src/main/java/main/services/AuthenticationService.java
+++ src/main/java/main/services/AuthenticationService.java
@@ -28,10 +28,11 @@ public class AuthenticationService {
     }
 
     public User signup(RegisterUserDto input) {
-        User user = new User()
-                .setFullName(input.getFullName())
-                .setEmail(input.getEmail())
-                .setPassword(passwordEncoder.encode(input.getPassword()));
+        User user = new User();
+
+                user.setFullName(input.getFullName());
+                user.setEmail(input.getEmail());
+                user.setPassword(passwordEncoder.encode(input.getPassword()));
 
         return userRepository.save(user);
     }
