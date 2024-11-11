4e1dab4b32873dfaf903b74ba71b89f32677d77c
Alex Balak <alex.balak@outlook.com>
2024-11-11 19:42:33 +0100

    Working need to implement refresh token to the controller

--- src/main/java/main/services/JwtService.java
+++ src/main/java/main/services/JwtService.java
@@ -50,6 +50,10 @@ public class JwtService {
         return buildToken(extraClaims, userDetails, jwtExpiration, secretKey);
     }
 
+    public long getExpirationTime() {
+        return jwtExpiration;
+    }
+
     public String generateRefreshToken(UserDetails userDetails) {
         return buildToken(new HashMap<>(), userDetails, refreshExpiration, refreshSecretKey);
     }
